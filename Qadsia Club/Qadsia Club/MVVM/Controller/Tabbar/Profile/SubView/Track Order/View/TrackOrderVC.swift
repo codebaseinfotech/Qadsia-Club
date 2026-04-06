71ec7a82118ba47b998b4bcf0a7dc516cbb9aa25
Ankit Gabani <ankitgabani@mobiiworld.com>
2026-04-06 16:39:00 +0530

    Track order working

---
+++ Qadsia Club/Qadsia Club/MVVM/Controller/Tabbar/Profile/SubView/Track Order/View/TrackOrderVC.swift
@@ -0,0 +1,22 @@
+//
+//  TrackOrderVC.swift
+//  Qadsia
+//
+//  Created by Poojagabani on 06/04/26.
+//
+
+import UIKit
+
+class TrackOrderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tappedBack(_ sender: Any) {
+    }
+    
+
+}
